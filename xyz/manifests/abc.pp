bc63aa9bb21b6175c352a2bde90f19592492bd38
MahenderDevOpsGit <mahender.p612@gmail.com>
2018-01-04 10:03:40 +0530

    modified new file for inherits

---
+++ xyz/manifests/abc.pp
@@ -0,0 +1,10 @@
+#class xyz::abc inherits xyz{
+class xyz::abc{
+          $course='devops'
+           notify { "Course is ${course}": }
+           notify { "The game is using class xyz ${xyz::test} ": }
+           notify { "The game is usng direct xyz var : ${test} ": }
+           notify { "The global value of  game is from init ${xyz::game} ": }
+           notify { "The global value of  game is ${game} ": }
+
+}
---
+++ xyz/manifests/init.pp
@@ -0,0 +1,8 @@
+class xyz inherits xyz::abc{
+           $game='golf'
+           $test='localgolf'
+          notify {
+                  "game: ${game}":
+                 }
+ notify { "The value is from xyz call abc var: ${course} ": }
+ }
